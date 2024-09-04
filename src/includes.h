20e3191556ffe634703db4e6b1a4f3ba3c8cc3c1
RBashkort <skrzkrv@gmail.com>
2024-09-04 22:04:32 +0500

    Added new files main.h & includes.h

---
+++ src/includes.h
@@ -0,0 +1,5 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <math.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,5 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-
-GLfloat points[] = {
-     0.0f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f
-};
-GLfloat colors[] = {
-    1.0f,  0.0f, 0.0f,
-    0.0f,  1.0f, 0.0f,
-    0.0f,  0.0f, 1.0f
-};
-
-const char* vertex_shader = 
-"#version 460\n"
-"layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color;"
-"out vec3 color;"
-"void main() {"
-"   color = vertex_color;"
-"   gl_Position = vec4(vertex_position, 1.0);"
-"};"; 
-
-const char* fragment_shader = 
-"#version 460\n"
-"in vec3 color;"
-"out vec4 frag_color;"
-"void main() {"
-"   frag_color = vec4 (color, 1.0);"
-"};";
+#include "includes.h"
+#include "main.h"
 
 int g_WindowSizeX = 640; // screen Width
 int g_WindowSizeY = 480; // screen Height
